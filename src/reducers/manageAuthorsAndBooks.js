10a6572aba8c498aa6773f7286c00fb8c0e1db07
Veronica Bustabad <vbustabad@gmail.com>
2018-11-18 22:28:15 +0000

    automatically backed up by learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -28,11 +28,11 @@ function authorsReducer(state = [], action) {
   switch (action.type) {
     case "ADD_AUTHOR":
       return [...state, action.author];
- 
+
     case "REMOVE_AUTHOR":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
- 
+
     case "ADD_BOOK":
       let existingAuthor = state.filter(
         author => author.authorName === action.book.authorName
@@ -42,7 +42,7 @@ function authorsReducer(state = [], action) {
       } else {
         return [...state, { authorName: action.book.authorName, id: uuid() }];
       }
- 
+
     default:
       return state;
   }
