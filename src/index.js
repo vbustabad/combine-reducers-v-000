70a9dd5429579b8a1940db68479f519b0080269f
Veronica Bustabad <vbustabad@gmail.com>
2018-11-18 22:14:07 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from "./reducers/manageAuthorsAndBooks";
- 
+
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
